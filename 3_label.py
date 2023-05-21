bb574d4d2a2a3f676e0f0850ac9239e608fb8a43
dongpakka <kimvjgd@naver.com>
2023-05-21 14:48:35 +0900

    works in window

--- 3_label.py
+++ 3_label.py
@@ -1,4 +1,6 @@
 from tkinter import *
+
+from PIL import Image, ImageTk
 import os
 
 def resource_path(relative_path):
@@ -15,8 +17,15 @@ root.geometry("640x480")
 label1 = Label(root, text="안녕하세요")
 label1.pack()
 
-photo1 = PhotoImage(file=resource_path('img/1.png'))
-photo2 = PhotoImage(file=resource_path('img/temp/2.png'))
+# temp_img = Image.open(resource_path('img/1.png'))
+temp_img = Image.open(resource_path('img/1.png'))
+resized_temp_img = temp_img.resize((20, 20), Image.ANTIALIAS)
+
+# photo1 = PhotoImage(file=resource_path('img/1.png'))
+photo1 = ImageTk.PhotoImage(resized_temp_img)
+# photo1.config(width=20, height=20)
+photo2 = PhotoImage(file=resource_path('img/2.png'))      
+# 그냥 path를 적어줘야하는데... 후..
 
 
 
@@ -33,3 +42,4 @@ btn = Button(root, text='클릭', command= change)
 btn.pack()
 
 root.mainloop()
+
